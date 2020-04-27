fa1596732f64cfbee1ab0e20c15e0bff64d29310
LNG <lng@Liyangs-MacBook-Pro-2017.local>
2020-04-27 16:38:34 -0700

    added test middleware and testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { indexPage, aboutPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage } from '../middleware';
+
 const indexRouter = express.Router();
 
 indexRouter.get('/messages', messagesPage);
 indexRouter.get('/', indexPage);
 indexRouter.get('/about', aboutPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
