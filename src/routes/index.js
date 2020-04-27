6bf0eb987cc82f37e9148b23bcb1a1418385f5c6
LNG <lng@Liyangs-MacBook-Pro-2017.local>
2020-04-27 15:52:55 -0700

    added message route and testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { indexPage, aboutPage, messagesPage } from '../controllers';
+import { indexPage, aboutPage, messagesPage, addMessage } from '../controllers';
 const indexRouter = express.Router();
 
 indexRouter.get('/messages', messagesPage);
 indexRouter.get('/', indexPage);
 indexRouter.get('/about', aboutPage);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
